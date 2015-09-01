945150a31fd4d9d8e1b817ae187286057c03d9c3
Arek van Schaijk <info@ucreation.nl>
2015-09-01 14:45:08 +0200

    Base kickstart version

---
+++ ext_tables.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Pi1',
+	'Properties'
+);
+
+if (TYPO3_MODE === 'BE') {
+
+	/**
+	 * Registers a Backend Module
+	 */
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+		'Ucreation.' . $_EXTKEY,
+		'tools',	 // Make module a submodule of 'tools'
+		'm1',	// Submodule key
+		'',						// Position
+		array(
+			
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_m1.xlf',
+		)
+	);
+
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Arek van Schaijk');
